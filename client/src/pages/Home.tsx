e97a9df6ce487c0729fce69212db62bcad52bc2f
SamuelRicardoDS <samuelricardods@gmail.com>
2022-10-09 17:41:01 -0300

    trying to connect server to mysql

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,24 @@
+import React, { useState } from 'react';
+
 export const Home = () => {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+    
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        console.log({ name, password });
+    }
+
     return (
         <div className="">
-            <h1 className="bg-slate-600">Home</h1>
+            <h1 className="text-center">Home</h1>
+            <form onSubmit={handleSubmit} className="flex flex-col w-1/4 items-center bg-slate-400 p-5 mt-3">
+                <h1>registro</h1>
+                    <input type="text" placeholder="nome" onChange={(e) => setName(e.target.value)} className="mt-2"/>
+                    <input type="password" placeholder="senha" onChange={(e) => setPassword(e.target.value)} className="mt-2"/>
+                    <button className="bg-slate-500 text-white p-2 mt-2">Registrar</button>
+            </form> 
         </div>
     )
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
+const mysql = require("mysql")
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+const database = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    database: "MySQL Model"
+})
+
+app.get("/", (req, res) => {
+    let SQL = "INSERT INTO users (name, password) VALUES ('Samuel', '123456')"
+    database.query(SQL, (err, result) => {
+        console.log(err)
+    })
+});
+
+app.listen(3001, () => {
+    console.log('Listening on port 3001');
     });
