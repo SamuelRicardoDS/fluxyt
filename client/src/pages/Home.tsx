373158a9b8055a32d1f84b2bf4aed59ac47125a9
SamuelRicardoDS <samuelricardods@gmail.com>
2022-10-11 15:34:22 -0300

    can insert in fluxyDB

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,11 +3,12 @@ import React, { useState } from 'react';
 export const Home = () => {
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
 
     
     const handleSubmit = (e: { preventDefault: () => void; }) => {
         e.preventDefault();
-        console.log({ name, password });
+        console.log({ name, email, password });
     }
 
     return (
@@ -16,6 +17,7 @@ export const Home = () => {
             <form onSubmit={handleSubmit} className="flex flex-col w-1/4 items-center bg-slate-400 p-5 mt-3">
                 <h1>registro</h1>
                     <input type="text" placeholder="nome" onChange={(e) => setName(e.target.value)} className="mt-2"/>
+                    <input type="email" placeholder="email" onChange ={(e) =>setEmail(e.target.value)}className="mt-2"/>
                     <input type="password" placeholder="senha" onChange={(e) => setPassword(e.target.value)} className="mt-2"/>
                     <button className="bg-slate-500 text-white p-2 mt-2">Registrar</button>
             </form> 
--- server/index.js
+++ server/index.js
@@ -4,11 +4,10 @@ const app = express();
 const database = require('./models/db');
 
 app.get("/", (req, res) => {
-   /*  let SQL = "INSERT INTO users (name, password) VALUES ('Samuel', '123456')"
+    let SQL = "INSERT INTO users (nome, email, senha) VALUES ('Samuel', 'samucardiass@gmail.com', 'Macacorosa?1' )";
     database.query(SQL, (err, result) => {
         console.log(err)
-    }) */
-    res.send("Hello World! - fluxy")
+    })
 });
 
 app.listen(3001, () => {
