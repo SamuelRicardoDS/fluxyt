9231943a3b482574b1ba1e4ad2a47ebbcaae59cb
SamuelRicardoDS <samuelricardods@gmail.com>
2022-10-13 15:27:25 -0300

    esqueletos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
+import { Login } from './pages/Login'
 import "./main.css"
+import { Home } from './pages/Home'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
       
