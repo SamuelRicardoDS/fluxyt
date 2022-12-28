c23dbe28817aa51907fad9b879fea2840ed1ed36
SamuelRicardoDS <samuelricardods@gmail.com>
2022-12-28 20:21:48 -0300

    problem(react): json dropdown courses is inline

--- client/src/components/RegisterSemester.tsx
+++ client/src/components/RegisterSemester.tsx
@@ -1,3 +1,5 @@
+import Cursos from '../data/cursos.json'
+
 export const RegisterSemester = () => {
     const handleclick = (e: { preventDefault: () => void; }) => {
         e.preventDefault();
@@ -7,10 +9,15 @@ export const RegisterSemester = () => {
     return(
         <div className="flex justify-center">
             <form action="" className="border flex flex-col w-1/4 p-5">
-                <input type="text" placeholder="número do semestre"/>
+                <select name="semester" id="">
+                    <option value="">1</option>
+                </select>
                 <div className="flex flex-row justify-evenly">
                     <select name="" id="">
-                        <option value="calculo">calculo</option>
+                        <option selected disabled={true}>selecione uma materia</option>
+                        {
+                            Cursos.map((curso)=> (<option value={curso.materias[1].obrigatorias}>{curso.materias[1].obrigatorias}</option>))
+                        }
                     </select>
                     <button onClick={(e) => handleclick(e)}>mais</button>
                 </div>
