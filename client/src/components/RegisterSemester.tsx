0a859008cf6262c6224fbdce438e895178f4a939
SamuelRicardoDS <samuelricardods@gmail.com>
2023-01-04 21:07:48 -0300

    feat: cadastro de semestre

--- client/src/components/RegisterSemester.tsx
+++ client/src/components/RegisterSemester.tsx
@@ -1,17 +1,19 @@
 import Cursos from '../data/cursos.json'
+import { useState } from 'react';
 
 export const RegisterSemester = () => {
     const handleclick = (e: { preventDefault: () => void; }) => {
         e.preventDefault();
         console.log("clicou")
+        setSemester(semester + 1)
     }
+
+    const [semester, setSemester] = useState(1)
 
     return(
         <div className="flex justify-center">
             <form action="" className="border flex flex-col w-1/4 p-5">
-                <select name="semester" id="">
-                    <option value="">1</option>
-                </select>
+                <h1>Semestre a ser cadastrado: {semester}</h1>
                 <div className="flex flex-row justify-evenly">
                     <select name="" id="">
                         <option selected disabled={true}>selecione uma materia</option>
@@ -19,9 +21,11 @@ export const RegisterSemester = () => {
                             Cursos.map((curso) => curso.materias[1].obrigatorias.map((obrigatoria) => <option value={obrigatoria}>{obrigatoria}</option>))
                         }
                     </select>
-                    <button onClick={(e) => handleclick(e)}>mais</button>
+                    <button>mais</button>
                 </div>
                 <input type="text" placeholder="ira"/>
+                <button onClick={(e) => handleclick(e)}>cadastrar semestre</button>
+
             </form>
         </div>
     )
